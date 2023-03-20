6b2cac5362afc73ff209ea910df62600ca65dd02
Jeremy Faivre <contact@jeremyfa.com>
2023-03-20 17:10:01 +0100

    Better thumbnail generation

--- build.sh
+++ build.sh
@@ -6,5 +6,12 @@ cd content
 find . -name '.gitignore' -exec rm {} +
 cd ..
 
+# Get latest generated samples
+if [ -d _samples ]; then
+    rm -rf _samples
+fi
+git clone --branch gh-pages https://github.com/ceramic-engine/ceramic-samples.git _samples
+
+
 # Run eleventy
 npx @11ty/eleventy
